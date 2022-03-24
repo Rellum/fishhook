e51ce7f7ba6d64cbbfc04fffb5f0d03b9df55039
Vaughan Muller <vaughan.github@muller.pw>
2022-03-24 17:26:10 +0100

    Implement forbidden words hook

--- hooks/forbidden-words/main.go
+++ hooks/forbidden-words/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 const (
 	// exitFail is the exit code if the program fails.
 	exitFail = 1
 )
+
+var forbidden = []string{
+	"nocommit",
+	"console.log(",
+	"debugger",
+}
 
 func main() {
 	if err := run(os.Args, os.Stdout); err != nil {
@@ -19,7 +27,35 @@ func main() {
 }
 
 func run(args []string, stdout io.Writer) error {
-	// TODO: Implement me!
+	for i := range args[1:] {
+		checkFile(args[i])
+	}
+
+	return nil
+}
+
+func checkFile(name string) error {
+	file, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("read file error: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var i int
+	for scanner.Scan() {
+		i++
+		text := scanner.Text()
+
+		for j := range forbidden {
+			idx := strings.Index(text, forbidden[j])
+			if idx >= 0 {
+				return fmt.Errorf("forbidden string '%s' found in %s:%v col %v", forbidden[j], name, i, idx)
+			}
+		}
+	}
 
 	return nil
 }
